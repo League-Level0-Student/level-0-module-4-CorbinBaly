421623293b21827c39d71cdb058dcfe930baa138
league <league@wts-im-17.attlocal.net>
2018-08-16 19:57:29 -0700

    Finished HappyPet and started PiAloud

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -1,30 +1,64 @@
 package charAt_scanners_optionDialogs;
+
 import javax.swing.JOptionPane;
+import javax.xml.bind.ParseConversionEvent;
 
 public class HappyPet {
-	
-	// 2. Add the following variable to the next line: static int happinessLevel = 0;
-	// this will be used to store the happiness of your pet
-	
-	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
 
+	// 2. Add the following variable to the next line:
+	static int happinessLevel = 0;
+
+	// this will be used to store the happiness of your pet
+	public static void main(String[] args) {
+
+		// 1. Ask the user what kind of pet they want to buy, and store their answer in
+		// a variable
+		String Pet = JOptionPane.showInputDialog("what pet do you want to buy?");
 		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
-		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+		for (int i = 0; i < 5; i++) {
+
+			// 3. Use showOptionDialog to ask the user what they want to do to make their
+			// pet happy
+			// (eg: cuddle, food, water, take a walk, groom, clean up poop).
+			// Make sure to customize the title and question too.
+			int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?",
+					"Pet Happiness", 0, JOptionPane.INFORMATION_MESSAGE, null,
+					new String[] { "Groom", "Give it food", "Hug it" }, null);
 
 			// 5. Use user input to call the appropriate method created in step 4.
+			if (task == 1) {
+				feedAnimal();
 
+			} else if (task == 2) {
+			petAnimal();
+			} else {
+				groomAnimal();
+			}
 			// 6. If you determine the happiness level is large enough, tell the
-			//    user that he loves his pet and use break; to exit for loop.
-
+			// user that he loves his pet and use break; to exit for loop.
+			if (happinessLevel >= 10) {
+				break;
+			}
+		}
 	}
 
 	// 4. Create methods to handle each of your user selections.
-	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
-	//    and INCREMENT the pet's happiness Level.
+	// Each method should create a pop-up with the pet's response (eg. cat might
+	// purr when pet),
+	// and INCREMENT the pet's happiness Level.
+	static void groomAnimal() {
+		JOptionPane.showMessageDialog(null, "Your pet loved the grooming");
+		happinessLevel = happinessLevel + 2;
+
+	}
+
+	static void petAnimal() {
+		JOptionPane.showMessageDialog(null, "Your pet liked that but didn't really feel like being hugged");
+		happinessLevel = happinessLevel + 1 / 2;
+	}
+
+	static void feedAnimal() {
+		JOptionPane.showMessageDialog(null, "Your pet was very hungry and loved the food");
+		happinessLevel = happinessLevel + 4;
+	}
 }
--- src/charAt_scanners_optionDialogs/PiAloud.java
+++ src/charAt_scanners_optionDialogs/PiAloud.java
@@ -9,9 +9,9 @@ import java.util.Scanner;
 public class PiAloud {
 
 	// 1. Make a main method and make sure your program can run
-
+	public static void main(String[] args) {
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
-
+ String Pi = "3.1415926535";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
 
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
@@ -22,7 +22,7 @@ public class PiAloud {
 	// *6. Get a character from the user using the getInputFromUser() method
 	// *7. Compare the users' char to the next digit of Pi
 	// *8. If they are correct, print out "correct". If they are not, print "incorrect" to System.err.println
-
+	}
 	static void speak(char characterToSpeak) {
 		try {
 			Runtime.getRuntime().exec("say " + characterToSpeak).waitFor();
@@ -44,7 +44,6 @@ public class PiAloud {
 		Scanner scanner = new Scanner(System.in);
 		return scanner.next().toCharArray()[0];
 	}
-
 }
 
 
