87d3a8d1f3639e8c573c0ef3778f24b26f0ac4ae
league <league@wts9.attlocal.net>
2018-08-29 19:59:55 -0700

    Finished ChangeCalculator,Googly Eyes, started sleephead.

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,17 +15,25 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
+String nick = JOptionPane.showInputDialog("How many nickels do you have?");
 
 		// Convert their answer to an int using Integer.parseInt()
-
+int nickel= Integer.parseInt(nick);
+nickel=nickel*5;
 		// Ask the user how many dimes they have, and convert their answer
-
+String dim=JOptionPane.showInputDialog(null, "How many dimes do you have?");
+int dime=Integer.parseInt(dim);
+dime=dime*10;
 		// Ask the user how many quarters they have, and convert their answer
-
+String quart=JOptionPane.showInputDialog(null, "How many quarters do you have?");
+int quarter=Integer.parseInt(quart);
+quarter=quarter*25;
 		// Calculate how much money the user has and save it in a double variable 
+int Money=quarter+dime+nickel;
+System.out.println(Money);
 
 		// Tell the user how much money they have
-
+JOptionPane.showMessageDialog(null, "You have " +Money+" cents");
 	}
 }
 
