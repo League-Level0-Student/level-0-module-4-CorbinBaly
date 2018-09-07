65d9bc709ea66baccff5bd4d2fc602ffd3d43016
league <league@wts5.attlocal.net>
2018-09-06 20:01:15 -0700

    finished sleepyhead,PiAloud

--- src/charAt_scanners_optionDialogs/PiAloud.java
+++ src/charAt_scanners_optionDialogs/PiAloud.java
@@ -11,11 +11,17 @@ public class PiAloud {
 	// 1. Make a main method and make sure your program can run
 	public static void main(String[] args) {
 	// 2. Make a String variable to hold the value of Pi. You could use http://www.piday.org/million/ for the value.
- String Pi = "3.1415926535";
+ String pi = "3.14159265358979323846";
 	// 3. Print out some digits of Pi. The first value is "pi.charAt(0)", the second is "pi.charAt(1)"
-
+ 		System.out.println(pi.charAt(4));
+ 		System.out.println(pi.charAt(5));
+ 		System.out.println(pi.charAt(6));
+ 		System.out.println(pi.charAt(7));
 	// 4. Print ALL the digits of of Pi (hint: use a loop)
-
+   for (int i = 0; i < pi.length(); i++) {
+	System.out.println(pi.charAt(i));
+speak(pi.charAt(i));
+}
 	// 5. Use the speak() method to speak all the digits of Pi.
 
 	// [ADVANCED]
--- src/doubles_and_booleans/SleepyHead.java
+++ src/doubles_and_booleans/SleepyHead.java
@@ -1,39 +1,58 @@
 package doubles_and_booleans;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
+
 //    Level 0
 
 import javax.swing.JOptionPane;
 
-
 public class SleepyHead {
 
+	public static void main(String[] args) {
 
-    public static void main(String[] args) {
-        
-        boolean isWeekday, isVacation;
-        
-        /* You MUST use the above boolean variables in your code */
-        
-        /*
-         * Ask the user for these values using a confirm dialog like the one below
-         */
- int day= JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-if(day==JOptionPane.YES_OPTION) {
-	isWeekday=true;
-	int vac= JOptionPane.showConfirmDialog(null, "Is it a vacation?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
-	else if(isVacation==YES_OPTION) {
-	
-else {
-	isWeekday=false;
-	System.out.println("Sleep in?");
+		boolean isWeekday, isVacation;
+		isWeekday = false;
+		isVacation = false;
+		/* You MUST use the above boolean variables in your code */
+
+		/*
+		 * Ask the user for these values using a confirm dialog like the one below
+		 */
+		int day = JOptionPane.showConfirmDialog(null, "Is it a weekday?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+		if (day == JOptionPane.YES_OPTION) {
+			isWeekday = true;
+		}
+		if(day==JOptionPane.NO_OPTION) {
+			JOptionPane.showMessageDialog(null, "Sleep in?");
+			System.exit(0);
+		}
+
+		else if (day == JOptionPane.NO_OPTION) {
+			JOptionPane.showMessageDialog(null, "Sleep in?");
+			System.exit(0);
+		}
+		int vac = JOptionPane.showConfirmDialog(null, "Is it vacation?", "Sleepy Head", JOptionPane.YES_NO_OPTION);
+		 if (vac == JOptionPane.YES_OPTION) {
+			isVacation = true;
+			JOptionPane.showMessageDialog(null, "Sleep in?");
+
+		}
+ 
+		if (isWeekday == true && isVacation == false) {
+			JOptionPane.showMessageDialog(null, "Get up sleepyhead!!");
+			System.exit(0);
+
+		}
+		else if (vac == JOptionPane.NO_OPTION) {
+			JOptionPane.showMessageDialog(null, "Get up lazybones!!");
+			System.exit(0);
+		}
+
+		
+		/*
+		 * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday, print
+		 * “get up lazybones!�? If it is a weekday, and we are on vacation, print “sleep
+		 * in�?.
+		 */
+
+	}
 }
-
-}
-
-        /*
-         * Print “sleep in�? if it is a vacation or a weekend. If it’s a weekday,
-         * print “get up lazybones!�? If it is a weekday, and we are on vacation,
-         * print “sleep in�?.
-         */
-    }
-
